238df193a17e274566f08a376fcc7c1540e321dc
Mariano Guerra <mariano@marianoguerra.org>
2013-08-15 10:02:59 +0200

    make shuffle work on streams

--- commands/shuffle.py
+++ commands/shuffle.py
@@ -1,19 +1,20 @@
-import sys
-from yel_utils import TypeCommand
-import collections
+from yel_utils import InputCommand
 import random
 
-class Command(TypeCommand):
+class Command(InputCommand):
 
     def __init__(self, options, din, dout):
-        TypeCommand.__init__(self, collections.Iterable, self.fun,
-                options, din, dout)
+        InputCommand.__init__(self, options, din, dout)
+        self.accum = []
 
-    def fun(self, data):
-        items = list(data)
-        random.shuffle(items)
-        return items
+    def on_end(self):
+        random.shuffle(self.accum)
+        for item in self.accum:
+            self.printer(item)
 
-def run(options=None, din=sys.stdin, dout=sys.stdout):
+    def on_data(self, data):
+        self.accum.append(data)
+
+def run(options, din, dout):
     cmd = Command(options, din, dout)
     return cmd.run()
